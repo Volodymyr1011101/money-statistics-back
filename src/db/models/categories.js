a3cd92304da684990ff9e28014e1284505a460d9
EugeneyCh <eugeney@ua.fm>
2025-05-17 21:24:35 +0300

    createCategories

---
+++ src/db/models/categories.js
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ['income', 'expense'],
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const CategoryModel = model('category', categorySchema);
+
+export default CategoryModel;
