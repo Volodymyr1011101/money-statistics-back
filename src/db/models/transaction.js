bef884be2d45aafbc8d847fb9db5a0178a91a238
Olexander <dneprovsky1997@icloud.com>
2025-05-21 18:27:07 +0200

    WIP: локальні зміни перед оновленням з remote

--- src/db/models/transaction.js
+++ src/db/models/transaction.js
@@ -1,41 +1,40 @@
-import {model, Schema} from 'mongoose';
+import { model, Schema } from 'mongoose';
 
-import {handleServerError, setUpdateSettings} from './hooks.js';
-import {transactionType} from '../../constants/index.js';
+import { handleServerError, setUpdateSettings } from './hooks.js';
+import { transactionType } from '../../constants/index.js';
 
 const transactionSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: transactionType,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  sum: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 1000000,
+  },
+  date: {
+    type: Date,
+    required: true,
+    set: (val) => {
+      return typeof val === 'string' ? new Date(`${val}T00:00:00.000Z`) : val;
     },
-    type: {
-        type: String,
-        enum: transactionType,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    sum: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 1000000,
-    },
-    date: {
-        type: Date,
-        required: true,
-        set: (val) => {
-            return typeof val === 'string' ? new Date(`${val}T00:00:00.000Z`) : val;
-        },
-    },
-    comment: {
-        type: String,
-        required: false,
-        default: '',
-    },
-
+  },
+  comment: {
+    type: String,
+    required: false,
+    default: '',
+  },
 });
 
 transactionSchema.post('save', handleServerError);
